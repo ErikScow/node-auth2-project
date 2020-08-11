9a7e96c27315925a3fc8c1a3eea177001e0be9a6
Erik Scow <erikscow@gmail.com>
2020-08-10 17:52:38 -0700

    Added dependencies, file structure, database, and started writing server.

---
+++ data/migrations/20200810175023_create-users-table.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('users', tbl => {
+      tbl.increments()
+      tbl.text('username')
+          .unique()
+          .notNullable()
+      tbl.text('password')
+          .notNullable()
+    })
+  };
+  
+  exports.down = function(knex) {
+    knex.schema.dropTableIfExists('users')
+  };
+  
---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const server = express()
+
+server.use(express.json())
+
+server.post('/api/register', (req, res) => {
+
+})
+
+server.post('/api/login', (req, res) => {
+
+})
+
+server.get('/api/users', (req, res) => {
+
+})
+
+server.listen(5000, () => {
+    console.log(`server listening on port 5000`)
+})
